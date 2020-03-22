d58de91f83f8401320901b19c0bc87a4cf5ef193
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-03-22 02:01:43 +0100

    I updated the code of my create function in controllers to check if I can get data from body on post request.

--- controllers/api/v1/messages.js
+++ controllers/api/v1/messages.js
@@ -1,8 +1,14 @@
-const create = (req, res) => {
-    res.json({
-        "status": "success",
-        "message": "Posting a new message for user Pikachu"
-    });
+const Message = require('../../../models/Message');
+
+const create = (req, res, next) => {
+    console.log(req.body);
+
+    let message = new Message();
+    message.text = req.body.text;
+    message.user = req.body.user;
+
+    console.log(message.text);
+    console.log(message.user);
 }
 
 module.exports.create = create;
