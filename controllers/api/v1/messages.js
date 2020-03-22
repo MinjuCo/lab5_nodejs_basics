aaa16a46b26430be967f4a6b09ab1915ce606b66
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-03-22 05:03:44 +0100

    Changed the post request: get data from messages in body

--- controllers/api/v1/messages.js
+++ controllers/api/v1/messages.js
@@ -57,8 +57,8 @@ const getId = (req, res) => {
 
 const create = (req, res, next) => {
     let message = new Message();
-    message.text = req.body.text;
-    message.user = req.body.user;
+    message.text = req.body.message.text;
+    message.user = req.body.message.user;
 
     message.save((err, doc) => {
         if(err){
