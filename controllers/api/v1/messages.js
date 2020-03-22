13ad27c18a431e29554f7647412b2bc130d85006
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-03-22 02:12:55 +0100

    The function create in controllers can now send data to database and give a json response back.

--- controllers/api/v1/messages.js
+++ controllers/api/v1/messages.js
@@ -1,14 +1,26 @@
 const Message = require('../../../models/Message');
 
 const create = (req, res, next) => {
-    console.log(req.body);
-
     let message = new Message();
     message.text = req.body.text;
     message.user = req.body.user;
 
-    console.log(message.text);
-    console.log(message.user);
+    message.save((err, doc) => {
+        if(err){
+            res.json({
+                "status": "error",
+                "message": "Could not save message"
+            });
+        }else{
+            res.json({
+                "status": "success",
+                "message": {
+                    "user": doc.user,
+                    "text": doc.text
+                }
+            });
+        }
+    });
 }
 
 module.exports.create = create;
