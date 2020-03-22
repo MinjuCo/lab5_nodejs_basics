5baa9613151a3386e953597724825d0f8c9da4ab
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-03-22 02:34:43 +0100

    Created a route for get request of /api/v1/messages/:id

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const msgController = require('../../../controllers/api/v1/messages');
 
 router.get("/", msgController.getAll);
+router.get("/:id", msgController.getId);
 router.post("/", msgController.create);
 
 module.exports= router;
