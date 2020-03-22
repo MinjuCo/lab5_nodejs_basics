6f07f30b3562e5561339839179a514aadd6911de
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-03-22 04:58:08 +0100

    Updated the cors function in app.js and added link to codepen, app and portfolio in README file

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Building an API for live chat with nodejs. This is connected to my lab5 exercise.
 
 [Link to codepen](https://codepen.io/minju_98/pen/rNVZMjv)
+
+[Link to app](https://mj-nodejs-basics.herokuapp.com/api/v1/messages)
+
+[Link to portfolio](https://github.com/MinjuCo/webtech3-portfolio)
--- app.js
+++ app.js
@@ -23,12 +23,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1/messages', apiMessageRouter);
